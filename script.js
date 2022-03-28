968604be2ee9e9efa83d277cc271b20438eca4aa
bronickone <bronickone@gmail.com>
2022-03-28 18:55:25 +0300

    add operate function to script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let add = (a,b) => a + b;
+let add = (a, b) => a + b;
 
-let subtract = (a,b) => a - b;
+let subtract = (a, b) => a - b;
 
-let multiply = (a,b) => a * b;
+let multiply = (a, b) => a * b;
 
-let divide = (a,b) => a / b;
+let divide = (a, b) => a / b;
+
+function operate(operator, a , b){
+    if (operator === "+") return add(a, b)
+    if (operator === "-") return subtract(a, b)
+    if (operator === "*") return multiply(a, b)
+    if (operator === "/") return divide(a, b)
+}
